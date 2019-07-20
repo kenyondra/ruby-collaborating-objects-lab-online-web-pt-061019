6373f98d7a541658d55c1034e298c1ad51e20158
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-20 04:00:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,7 +28,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(song_name)
-    self.find(name) ? self.find(name) 
+    self.find(name) ? self.find(name)  
   end
   
   def print_songs 
